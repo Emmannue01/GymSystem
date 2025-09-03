67460f93d325499c64ff766dbd44f6f68a204a04
Emmannue01 <joabemmanue01@gmail.com>
2025-09-03 01:25:34 -0600

    Update config.js

--- JS/config.js
+++ JS/config.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyALeuGMHVzGuzEVejNcGx_9_MWEF5YHUx4",
-  authDomain: "colosossgym.firebaseapp.com",
-  databaseURL: "https://colosossgym-default-rtdb.firebaseio.com/",
-  projectId: "colosossgym",
-  storageBucket: "colosossgym.firebasestorage.app",
-  messagingSenderId: "498715862896",
-  appId: "1:498715862896:web:d44ff8eda38aa3493991b5",
-  measurementId: "G-H52V06S4QX"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
+
 export { firebaseConfig };
